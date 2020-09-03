3e96f0414b1c0285d4751b1aef08f1cb0510674f
BONG-HWI LIM <blim@bong-hwis-macbook-pro-2.home>
2020-09-03 20:12:50 +0200

    update run macro

--- run_KIAF.C
+++ run_KIAF.C
@@ -1,160 +1,130 @@
 #if !defined(__CINT__) || defined(__CLING__)
 #include "AliAODInputHandler.h"
 #include "AliAnalysisAlien.h"
+#include "AliPhysicsSelectionTask.h"
+#include "AliMultSelectionTask.h"
 #include "AliAnalysisManager.h"
+#include "AliMCEventHandler.h"
+#include "AliAnalysisTaskXi1530.h"
 #include "AliAnalysisTaskXi1530temp.h"
 #include "AliESDInputHandler.h"
-
-#include "./CreateESDChain.C"
 #endif
 
 void run_KIAF(const char* dataset = "test1.list",
-        const char* taskname = "Xi1530",
-         const char* option =
-             "LHC16k_pass2_SYS_Mix"  // when scanning AOD, add "AOD"
-         ,
-         const char* gridmode = "test"  // or "terminate" to merge
-         ,
-         UInt_t istart = 0,
-         UInt_t iend = 25,
-         const char* localorgrid = "local") {
-    gSystem->Load("libTree.so");
-    gSystem->Load("libGeom.so");
-    gSystem->Load("libVMC.so");
-    gSystem->Load("libPhysics.so");
-    gSystem->Load("libSTEERBase. so");
-    gSystem->Load("libESD.so");
-    gSystem->Load("libANALYSIS.so");
-    gSystem->Load("libOADB.so");
-    gSystem->Load("libANALYSISalice.so");
-    gSystem->Load("libpythia6_4_21.so");
+              const char* option = "AOD_Mix_Nano_SYS") {
+  gSystem->Load("libTree.so");
+  gSystem->Load("libGeom.so");
+  gSystem->Load("libVMC.so");
+  gSystem->Load("libPhysics.so");
+  gSystem->Load("libSTEERBase. so");
+  gSystem->Load("libESD.so");
+  gSystem->Load("libANALYSIS.so");
+  gSystem->Load("libOADB.so");
+  gSystem->Load("libANALYSISalice.so");
+  gSystem->Load("libpythia6_4_21.so");
 
-    gSystem->Setenv("alien_CLOSE_SE", "working_disk_SE");
-    // ROOT 6 MODE
-    // add aliroot indlude path
-    gInterpreter->ProcessLine(
-        Form(".include %s/include", gSystem->ExpandPathName("$ROOTSYS")));
-    gInterpreter->ProcessLine(
-        Form(".include %s/include", gSystem->ExpandPathName("$ALICE_ROOT")));
-    gInterpreter->ProcessLine(
-        Form(".include %s/include", gSystem->ExpandPathName("$ALICE_PHYSICS")));
+  gSystem->Setenv("alien_CLOSE_SE", "working_disk_SE");
+  // ROOT 6 MODE
+  // add aliroot indlude path
+  gInterpreter->ProcessLine(
+      Form(".include %s/include", gSystem->ExpandPathName("$ROOTSYS")));
+  gInterpreter->ProcessLine(
+      Form(".include %s/include", gSystem->ExpandPathName("$ALICE_ROOT")));
+  gInterpreter->ProcessLine(
+      Form(".include %s/include", gSystem->ExpandPathName("$ALICE_PHYSICS")));
 
-    bool isaa = kFALSE;
-    bool ismc = kFALSE;
-    bool setmixing = kFALSE;
-    bool vertexer = false;
-    int nmix = 20;
-    bool highmult = kFALSE;
-    TString foption = option;
-    const char* suffix = "test";
-    if (foption.Contains("MC"))
-        ismc = kTRUE;
-    if (foption.Contains("Vertex"))
-        vertexer = true;
-    const char* option2 =
-             "LHC16k_pass2_EXO";
-    const char* suffix2 = "EXO";
+  bool isaa = kFALSE;
+  bool ismc = kFALSE;
+  bool isDev = kFALSE;
+  bool isNano = kFALSE;
+  bool setmixing = kFALSE;
+  bool isaod = kFALSE;
+  int nmix = 10;
+  bool highmult = kFALSE;
+  TString foption = option;
+  const char* taskname = "Xi1530MB";
+  const char* suffix = "_";
+  if (foption.Contains("MC"))
+    ismc = kTRUE;
+  if (foption.Contains("Dev"))
+    isDev = kTRUE;
+  if (foption.Contains("AOD"))
+    isaod = true;
+  if (foption.Contains("Nano"))
+    isNano = true;
 
-    // analysis manager
-    AliAnalysisManager* mgr =
-        new AliAnalysisManager(Form("%s%s", taskname, option));
-    AliInputEventHandler* handler;
-    if (foption.Contains("AOD"))
-        handler = new AliAODInputHandler();
-    else
-        handler = new AliESDInputHandler();
+  // analysis manager
+  AliAnalysisManager* mgr =
+      new AliAnalysisManager(Form("%s%s", taskname, option));
+  AliInputEventHandler* handler;
+  if (isaod)
+    handler = new AliAODInputHandler();
+  else
+    handler = new AliESDInputHandler();
 
-    // handler->SetNeedField(1);
-    mgr->SetInputEventHandler(handler);
+  // handler->SetNeedField(1);
+  mgr->SetInputEventHandler(handler);
 
-    if (ismc) {
-        AliMCEventHandler* mcHandler = new AliMCEventHandler();
-        mgr->SetMCtruthEventHandler(mcHandler);
-    }
+  if (ismc) {
+    AliMCEventHandler* mcHandler = new AliMCEventHandler();
+    mgr->SetMCtruthEventHandler(mcHandler);
+  }
+  if(!isNano){
     //
     // Physics Selection
     AliPhysicsSelectionTask* physSelTask =
         reinterpret_cast<AliPhysicsSelectionTask*>(gInterpreter->ExecuteMacro(
             Form("$ALICE_PHYSICS/OADB/macros/AddTaskPhysicsSelection.C(%d)",
-                 ismc)));
-    if (!physSelTask) {
-        Printf("no physSelTask");
-        return;
-    }
+                ismc)));
     // Multiplicity selection
     AliMultSelectionTask* MultSlection =
         reinterpret_cast<AliMultSelectionTask*>(gInterpreter->ExecuteMacro(
             "$ALICE_PHYSICS/OADB/COMMON/MULTIPLICITY/macros/"
             "AddTaskMultSelection.C"));
-    if (!MultSlection) {
-        Printf("no MultSlection");
-        return;
-    }
     MultSlection->SetAddInfo(kTRUE);
     MultSlection->SetSelectedTriggerClass(AliVEvent::kAny);
     // MultSlection->SetAlternateOADBforEstimators("LHC16k"); //if needed
     // PID response
     AliAnalysisTask* fPIDResponse =
-        reinterpret_cast<AliAnalysisTaskXi1530temp*>(
-            gInterpreter->ExecuteMacro(Form(
-                "$ALICE_ROOT/ANALYSIS/macros/AddTaskPIDResponse.C(%d)", ismc)));
-    if (!fPIDResponse) {
-        Printf("no fPIDResponse");
-        return;
-    }
-    // V0, Xi Super verexter by David
-    if (vertexer) {
-        AliAnalysisTaskWeakDecayVertexer* taskWDV =
-            reinterpret_cast<AliAnalysisTaskWeakDecayVertexer*>(
-                gInterpreter->ExecuteMacro(
-                    "$ALICE_PHYSICS/PWGLF/STRANGENESS/Cascades/Run2/macros/"
-                    "AddTaskWeakDecayVertexer.C"));
-        taskWDV->SetUseImprovedFinding();
-
-        //V0-Related topological selections
-        taskWDV->SetV0VertexerDCAFirstToPV(0.05);
-        taskWDV->SetV0VertexerDCASecondtoPV(0.05);
-        taskWDV->SetV0VertexerDCAV0Daughters(1.5);
-        taskWDV->SetV0VertexerCosinePA(0.9);
-        taskWDV->SetV0VertexerMinRadius(0.2);
-        taskWDV->SetV0VertexerMaxRadius(200);
-
-        // Cascade-Related topological selections
-        taskWDV->SetCascVertexerMinV0ImpactParameter(0.01);
-        taskWDV->SetCascVertexerV0MassWindow(0.008);
-        taskWDV->SetCascVertexerDCABachToPV(0.01);
-        taskWDV->SetCascVertexerDCACascadeDaughters(2.0);
-        taskWDV->SetCascVertexerCascadeMinRadius(0.2);
-        taskWDV->SetCascVertexerCascadeMaxRadius(200);
-        taskWDV->SetCascVertexerCascadeCosinePA(0.98);
-
-        // Test1 track selection
-        //taskWDV-> SetExtraCleanup(kFALSE);
-        // Test2 pre-selection in dE/dx
-        //taskWDV-> SetUseMonteCarloAssociation(kFALSE);
-    }
+        reinterpret_cast<AliAnalysisTask*>(gInterpreter->ExecuteMacro(
+            Form("$ALICE_ROOT/ANALYSIS/macros/AddTaskPIDResponse.C(%d)", ismc)));
+  }
+  if(isNano)
+    gInterpreter->ExecuteMacro("$ALICE_PHYSICS/PWG/DevNanoAOD/macros/AddTaskNanoAODnormalisation.C");
+  if (isDev) {
     gInterpreter->LoadMacro("AliAnalysisTaskXi1530temp.cxx+g");
-    // AliAnalysisTaskXi1530temp *myTask =
-    // reinterpret_cast<AliAnalysisTaskXi1530temp*>(gInterpreter->ExecuteMacro(Form("AddTaskXi1530.c(\"%s\",\"%s\",%i,%d,%d,%d,%d)",taskname,option,nmix,highmult,isaa,ismc,setmixing)));
-
-    AliAnalysisTaskXi1530temp* myTask =
+    AliAnalysisTaskXi1530temp* task =
         reinterpret_cast<AliAnalysisTaskXi1530temp*>(gInterpreter->ExecuteMacro(
-            Form("AddTaskXi1530.C(\"%s\",\"%s\",%i,\"%s\")", taskname, option,
-                 nmix, suffix)));
-    /*
-    AliAnalysisTaskXi1530temp* myTask2 =
-        reinterpret_cast<AliAnalysisTaskXi1530temp*>(gInterpreter->ExecuteMacro(
-            Form("AddTaskXi1530.C(\"%s\",\"%s\",%i,\"%s\")", taskname, option2,
-                 nmix, suffix2)));
-    */
-    // mgr->SetDebugLevel(1);
-    if (!mgr->InitAnalysis())
-        return;
-    mgr->PrintStatus();
+            Form("AddTaskXi1530.C(\"%s\",\"%s\",%i)", taskname, option, nmix)));
 
-    // start analysis
-    Printf("Starting Analysis....");
+  } else {
+      AliAnalysisTaskXi1530* task =
+        reinterpret_cast<AliAnalysisTaskXi1530*>(gInterpreter->ExecuteMacro(
+            Form("$ALICE_PHYSICS/PWGLF/RESONANCES/PostProcessing/Xi1530/AddTaskXi1530.C(\"%s\",\"%s\",%i)", taskname, option, nmix)));
+  }
+  if (!mgr->InitAnalysis())
+    return;
+  mgr->PrintStatus();
 
-    TChain* chain = CreateESDChain(dataset,-1);
-    mgr->StartAnalysis("local",chain);
+  // start analysis
+  Printf("Starting Analysis....");
+
+  // TChain* chain = CreateESDChain(dataset,-1);
+  TChain* chain = new TChain("ESDTree");
+  std::stringstream esdChain;
+  if(isNano){
+    esdChain << ".x " << "CreateNanoAODChain.C(";
+    esdChain << "\"" << dataset << "\", -1);";
+  }
+  else if (isaod) {
+    esdChain << ".x " << gSystem->Getenv("ALICE_PHYSICS")
+             << "/PWG/EMCAL/macros/CreateAODChain.C(";
+    esdChain << "\"" << dataset << "\", -1);";
+  } else {
+    esdChain << ".x " << gSystem->Getenv("ALICE_PHYSICS")
+             << "/PWG/EMCAL/macros/CreateESDChain.C(";
+    esdChain << "\"" << dataset << "\", -1);";
+  }
+  chain = reinterpret_cast<TChain*>(gROOT->ProcessLine(esdChain.str().c_str()));
+  mgr->StartAnalysis("local", chain);
 }
